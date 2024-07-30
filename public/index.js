507776c5e00bc983e83bb963cf52361ba4cb4e9d
dev-Bambam <ayogood18@gmail.com>
2024-07-30 03:53:07 +0100

    finished the navbar, now moving to the hero section of the landing page

---
+++ public/index.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.querySelector('.theme-controller');
+    const htmlElement = document.documentElement;
+
+    // Retrieve the theme from local storage
+    const storedTheme = localStorage.getItem('theme');
+
+    // If a theme is stored, apply it
+    if (storedTheme) {
+        htmlElement.setAttribute('data-theme', storedTheme);
+    } else {
+        // If no theme is stored, default to 'dark'
+        htmlElement.setAttribute('data-theme', 'garden');
+        localStorage.setItem('theme', 'garden');
+    }
+
+    themeToggle.addEventListener('change', () => {
+        // Toggle the theme between 'dark' and 'garden'
+        if (themeToggle.checked) {
+            htmlElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            htmlElement.setAttribute('data-theme', 'garden');
+            localStorage.setItem('theme', 'garden');
+        }
+    })
+})
