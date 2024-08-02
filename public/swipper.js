3a29fd3a9e253a2913b8995df0e095bbddb35440
dev-Bambam <ayogood18@gmail.com>
2024-08-02 13:14:16 +0100

    moving on..

--- public/swipper.js
+++ public/swipper.js
@@ -1,25 +1,11 @@
-// // import Swiper JS
-// import Swiper from 'swiper';
-// // import Swiper styles
-// import 'swiper/css';
-
-// Ensure the DOM is fully loaded before initializing Swiper
 document.addEventListener('DOMContentLoaded', () => {
     // init Swiper:
     const swiper = new Swiper('.mySwiper', {
-        spaceBetween: 30,
+        spaceBetween: 0,
         centeredSlides: true,
         autoplay: {
             delay: 2500,
             disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+        }
     });
 });
