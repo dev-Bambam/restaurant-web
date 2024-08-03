4622dd3a668a3c67c097edfe8e3d272482edf490
dev-Bambam <ayogood18@gmail.com>
2024-08-03 12:31:02 +0100

    first commit of the day....now at testimonial section

--- public/swipper.js
+++ public/swipper.js
@@ -1,11 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // init Swiper:
+    // init Swiper: for hero section
     const swiper = new Swiper('.mySwiper', {
         spaceBetween: 0,
         centeredSlides: true,
         autoplay: {
             delay: 2500,
             disableOnInteraction: false,
+        }
+    });
+
+    // for testimonial section
+    const testimonialSwiper = new Swiper('.mySwiper3', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.testimonial-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            640: {
+                slidesPerView: 1,
+            }
         }
     });
 });
