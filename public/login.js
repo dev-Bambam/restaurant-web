feefa365e2ef3833fb6e4b1660bed26ff2a26f07
dev-Bambam <ayogood18@gmail.com>
2024-08-12 14:32:49 +0100

    done with the login page

--- public/login.js
+++ public/login.js
@@ -1,4 +1,7 @@
-document.querySelector('#form-page').addEventListener('submit', (e)=>{
+document.querySelector('#form').addEventListener('submit', (e)=>{
+
+    e.preventDefault()
+
     const fName = document.querySelector('#fName').value.trim()
     const lName = document.querySelector('#lName').value.trim()
     const email = document.querySelector('#email').value.trim()
@@ -10,4 +13,33 @@ document.querySelector('#form-page').addEventListener('submit', (e)=>{
     else{
         alert ('input all field')
     }
+})
+
+// theme swithcer
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.querySelector('.theme-controller');
+    const htmlElement = document.documentElement;
+
+    // Retrieve the theme from local storage
+    const storedTheme = localStorage.getItem('theme');
+
+    // If a theme is stored, apply it
+    if (storedTheme) {
+        htmlElement.setAttribute('data-theme', storedTheme);
+    } else {
+        // If no theme is stored, default to 'dark'
+        htmlElement.setAttribute('data-theme', 'garden');
+        localStorage.setItem('theme', 'garden');
+    }
+
+    themeToggle.addEventListener('change', () => {
+        // Toggle the theme between 'dark' and 'garden'
+        if (themeToggle.checked) {
+            htmlElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            htmlElement.setAttribute('data-theme', 'garden');
+            localStorage.setItem('theme', 'garden');
+        }
+    })
 })
