c828c857c914f7b71cea4965018c98b8eab6455e
dev-Bambam <ayogood18@gmail.com>
2024-08-15 21:19:33 +0100

    testing and debugging

--- public/index.js
+++ public/index.js
@@ -1,43 +1,43 @@
 // theme swithcer
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.querySelector('.theme-controller');
-    const htmlElement = document.documentElement;
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.querySelector(".theme-controller");
+  const htmlElement = document.documentElement;
 
-    // Retrieve the theme from local storage
-    const storedTheme = localStorage.getItem('theme');
+  // Retrieve the theme from local storage
+  const storedTheme = localStorage.getItem("theme");
 
-    // If a theme is stored, apply it
-    if (storedTheme) {
-        htmlElement.setAttribute('data-theme', storedTheme);
+  // If a theme is stored, apply it
+  if (storedTheme) {
+    htmlElement.setAttribute("data-theme", storedTheme);
+  } else {
+    // If no theme is stored, default to 'dark'
+    htmlElement.setAttribute("data-theme", "garden");
+    localStorage.setItem("theme", "garden");
+  }
+
+  themeToggle.addEventListener("change", () => {
+    // Toggle the theme between 'dark' and 'garden'
+    if (themeToggle.checked) {
+      htmlElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
     } else {
-        // If no theme is stored, default to 'dark'
-        htmlElement.setAttribute('data-theme', 'garden');
-        localStorage.setItem('theme', 'garden');
+      htmlElement.setAttribute("data-theme", "garden");
+      localStorage.setItem("theme", "garden");
     }
-
-    themeToggle.addEventListener('change', () => {
-        // Toggle the theme between 'dark' and 'garden'
-        if (themeToggle.checked) {
-            htmlElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            htmlElement.setAttribute('data-theme', 'garden');
-            localStorage.setItem('theme', 'garden');
-        }
-    })
-})
+  });
+});
 
 // place reservation
-const form = document.querySelector('#form')
-form.addEventListener('submit',()=>{
-    const lName = document.querySelector('#l-name').value.trim()
-    const fName = document.querySelector('#name').value.trim()
-    const email = document.querySelector('#email').value.trim()
-    const tel = document.querySelector('#tel').value.trim()
-    const userDate = document.querySelector('#user-date').value.trim()
-    const userTime = document.querySelector('#user-time').value.trim()
+const form = document.querySelector("#form");
+form.addEventListener("submit", () => {
+  const lName = document.querySelector("#l-name").value.trim();
+  const fName = document.querySelector("#name").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const tel = document.querySelector("#tel").value.trim();
+  const userDate = document.querySelector("#user-date").value.trim();
+  const userTime = document.querySelector("#user-time").value.trim();
 
-    if(lName && fName && email && tel && userDate && userTime){
-        alert('reservation placed: date:' )
-    }
-})
+  if (lName && fName && email && tel && userDate && userTime) {
+    alert("reservation placed: date:");
+  }
+});
--- public/login.js
+++ public/login.js
@@ -1,45 +1,49 @@
-document.querySelector('#form').addEventListener('submit', (e)=>{
+document.querySelector("#form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    e.preventDefault()
+  const fName = document.querySelector("#fName").value.trim();
+  const lName = document.querySelector("#lName").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
 
-    const fName = document.querySelector('#fName').value.trim()
-    const lName = document.querySelector('#lName').value.trim()
-    const email = document.querySelector('#email').value.trim()
-    const password = document.querySelector('#password').value.trim()
-    
-    if(fName && lName && email && password){
-        window.location.href = 'home.html'
-    }
-    else{
-        alert ('input all field')
-    }
-})
+  if (fName && lName && email && password) {
+    window.location.href = "home.html";
+  } else {
+    alert("input all field");
+  }
+});
 
 // theme swithcer
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.querySelector('.theme-controller');
-    const htmlElement = document.documentElement;
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.querySelector(".theme-controller");
+  const htmlElement = document.documentElement;
 
-    // Retrieve the theme from local storage
-    const storedTheme = localStorage.getItem('theme');
+  // Retrieve the theme from local storage
+  const storedTheme = localStorage.getItem("theme");
 
-    // If a theme is stored, apply it
-    if (storedTheme) {
-        htmlElement.setAttribute('data-theme', storedTheme);
+  // If a theme is stored, apply it
+  if (storedTheme) {
+    htmlElement.setAttribute("data-theme", storedTheme);
+  } else {
+    // If no theme is stored, default to 'dark'
+    htmlElement.setAttribute("data-theme", "garden");
+    localStorage.setItem("theme", "garden");
+  }
+
+  themeToggle.addEventListener("change", () => {
+    // Toggle the theme between 'dark' and 'garden'
+    if (themeToggle.checked) {
+      htmlElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
     } else {
-        // If no theme is stored, default to 'dark'
-        htmlElement.setAttribute('data-theme', 'garden');
-        localStorage.setItem('theme', 'garden');
+      htmlElement.setAttribute("data-theme", "garden");
+      localStorage.setItem("theme", "garden");
     }
+  });
+});
 
-    themeToggle.addEventListener('change', () => {
-        // Toggle the theme between 'dark' and 'garden'
-        if (themeToggle.checked) {
-            htmlElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            htmlElement.setAttribute('data-theme', 'garden');
-            localStorage.setItem('theme', 'garden');
-        }
-    })
-})
+// go back
+const goBackBtn = document.querySelector("#go-back");
+goBackBtn.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
